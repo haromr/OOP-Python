2dbad4bf0f5a589e59412002d09389ba024bdbc6
Harom Ramos <haromramos@gmail.com>
2018-05-18 15:03:34 -0400

    Update TiposDatosPython.py

--- TiposDatosPython.py
+++ TiposDatosPython.py
@@ -52,9 +52,65 @@ entero = 027
 # 0x17 = 23 en base 10
 entero =  0x17
 
+# String
+Cadenas=""" Las cadenas no son más que texto encerrado entre comillas simples (‘cadena’) o dobles (“cadena”). 
+Dentro de las comillas se pueden añadir caracteres especiales escapándolos con ‘\’, como ‘\n’, el carácter de nueva línea, o ‘\t’, el de tabulación."""
 
+# Las cadenas también admiten operadores como la suma (concatenación de cadenas) y la multiplicación.
 
-   
+a = "uno"
+b = "dos"
+c = a + b # c es "unodos"
+c = a * 3 # c es "unounouno"  
+
+# Comillas simples
+cadenaa = 'Texto entre comillas simples'
+print(cadenaa)
+print(type(cadenaa))
+
+# Comillas dobles
+cadenab = "Texto entre comillas dobles"
+print(cadenab)
+print(type(cadenab))
+
+# Cadena con codigo escapes
+cadenaesc = 'Texto entre \n\tcomillas simples'
+print(cadenaesc)
+print(type(cadenaesc))
+
+# Cadena multilinea
+cadenac = """Texto linea 1
+linea 2
+linea 3
+linea 4
+.
+.
+.
+.
+.
+linea N
+"""
+
+print(cadenac)
+print(type(cadenac))
+
+# Repeticion de cadena
+cadrep = "Cadena" * 3
+print cadrep
+print type (cadrep)
+
+# Concatenacion de cadena
+nombre = "Leonardo"
+apellido = "Caballero"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+print type((nombre_completo))
+
+print("Tamano de cadena '", nombre_completo, "' es:", len(nombre_completo))
+
+# acceder a rango de la cadena
+print(nombre_completo[3:13])
+
 
 
 
