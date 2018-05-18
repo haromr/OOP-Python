234d3c29b3319c8ed4930e8c1a20f858d2d514ca
Harom Ramos <haromramos@gmail.com>
2018-05-18 13:49:13 -0400

    Update TiposDatosPython.py

--- TiposDatosPython.py
+++ TiposDatosPython.py
@@ -1,1 +1,32 @@
+print("""
+En Python Existen 5 tipos simples de variables
+-----------------------------------------------
+int
+string
+Boolean
+List
+Dictionary
+""")
+
+# esto es una cadena
+c = "Hola Mundo"
+
+# y esto es un entero
+e = 23
+
+# La funcion type nos permite comprobar el tipo de una variable
+type(c)
+type(e)
+print(type(c))
+
+# Python no requiere que se defina el tipo de variable al momento de declararlas
+
+# De esta manera se declararian la variables en JAVA
+    # String c = "Hola Mundo";
+    # int e = 23;
+
+   
+
+
+
 
